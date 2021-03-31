c4fe25ea7d7f10ff3b60280a8d6caf0f1e6a635b
monkeyden <kylewburke@gmail.com>
2021-03-31 02:08:47 -0400

    Changes for testnet

--- index.js
+++ index.js
@@ -20,21 +20,8 @@ app.get('/', function(req, res) {
 
 app.get('/api/token/:token_id', function(req, res) {
   const tokenId = parseInt(req.params.token_id).toString()
-  const person = db[tokenId]
-  const bdayParts = person.birthday.split(' ')
-  const day = parseInt(bdayParts[1])
-  const month = parseInt(bdayParts[0])
-  const data = {
-    'name': person.name,
-    'attributes': {
-      'birthday': person.birthday,
-      'birth month': monthName(month),
-      'zodiac sign': zodiac(day, month),
-      // 'age': moment().diff(person.birthday, 'years')
-    },
-    'image': `${HOST}/images/${tokenId}.png`
-  }
-  res.send(data)
+  const gnome = db[tokenId]
+  res.send(gnome)
 })
 
 app.listen(app.get('port'), function() {
--- src/database.js
+++ src/database.js
@@ -1,22 +1,408 @@
 module.exports = {
-  "1": {
-    "name": "Chunky Monkey",
-    "birthday": "10-18"
-  },
-  "2": {
-    "name": "Flerp Derp",
-    "birthday": "01-08"
-  },
-  "3": {
-    "name": "Buttle McFuddle",
-    "birthday": "01-22"
-  },
-  "4": {
-    "name": "Quiero Pancakes",
-    "birthday": "03-04"
-  },
-  "5": {
-    "name": "Guino the Penguin",
-    "birthday": "03-09"
-  }
+	"1": {
+		"description": "Gnome 1 of 1000",
+		"image": "https://someserver.com?filename=Gnome_1_1.png",
+		"attributes": [
+			{
+				"trait_type": "Accessory",
+				"value": "sword"
+			},
+			{
+				"trait_type": "Background",
+				"value": "ethmoon"
+			},
+			{
+				"trait_type": "Attack",
+				"value": 84
+			},
+			{
+				"trait_type": "Defense",
+				"value": 21
+			},
+			{
+				"trait_type": "Health",
+				"value": 20
+			},
+			{
+				"trait_type": "Farming",
+				"value": 79
+			},
+			{
+				"trait_type": "Total Skill",
+				"value": 214
+			},
+			{
+				"display_type": "date",
+				"trait_type": "birthday",
+				"value": -4697350071
+			},
+			{
+				"display_type": "boost_number",
+				"trait_type": "Attack Boost",
+				"value": 10
+			}
+		]
+	},
+	"2": {
+		"description": "Gnome 2 of 1000",
+		"image": "https://someserver.com?filename=Gnome_1_2.png",
+		"attributes": [
+			{
+				"trait_type": "Accessory",
+				"value": "shield"
+			},
+			{
+				"trait_type": "Background",
+				"value": "ethmoon"
+			},
+			{
+				"trait_type": "Attack",
+				"value": 64
+			},
+			{
+				"trait_type": "Defense",
+				"value": 13
+			},
+			{
+				"trait_type": "Health",
+				"value": 72
+			},
+			{
+				"trait_type": "Magic",
+				"value": 44
+			},
+			{
+				"trait_type": "Total Skill",
+				"value": 233
+			},
+			{
+				"display_type": "date",
+				"trait_type": "birthday",
+				"value": -3435440617
+			},
+			{
+				"display_type": "boost_number",
+				"trait_type": "Defense Boost",
+				"value": 40
+			}
+		]
+	},
+	"3": {
+		"description": "Gnome 3 of 1000",
+		"image": "https://someserver.com?filename=Gnome_1_3.png",
+		"attributes": [
+			{
+				"trait_type": "Accessory",
+				"value": "pipe"
+			},
+			{
+				"trait_type": "Background",
+				"value": "vault"
+			},
+			{
+				"trait_type": "Attack",
+				"value": 72
+			},
+			{
+				"trait_type": "Defense",
+				"value": 70
+			},
+			{
+				"trait_type": "Health",
+				"value": 44
+			},
+			{
+				"trait_type": "Farming",
+				"value": 88
+			},
+			{
+				"trait_type": "Total Skill",
+				"value": 294
+			},
+			{
+				"display_type": "date",
+				"trait_type": "birthday",
+				"value": -4575377332
+			},
+			{
+				"display_type": "boost_number",
+				"trait_type": "Farming Boost",
+				"value": 20
+			}
+		]
+	},
+	"4": {
+		"description": "Gnome 4 of 1000",
+		"image": "https://someserver.com?filename=Gnome_1_4.png",
+		"attributes": [
+			{
+				"trait_type": "Accessory",
+				"value": "crossbow"
+			},
+			{
+				"trait_type": "Background",
+				"value": "ethmoon"
+			},
+			{
+				"trait_type": "Attack",
+				"value": 69
+			},
+			{
+				"trait_type": "Defense",
+				"value": 54
+			},
+			{
+				"trait_type": "Health",
+				"value": 71
+			},
+			{
+				"trait_type": "Building",
+				"value": 14
+			},
+			{
+				"trait_type": "Total Skill",
+				"value": 258
+			},
+			{
+				"display_type": "date",
+				"trait_type": "birthday",
+				"value": -6266434599
+			},
+			{
+				"display_type": "boost_number",
+				"trait_type": "Attack Boost",
+				"value": 50
+			}
+		]
+	},
+	"5": {
+		"description": "Gnome 5 of 1000",
+		"image": "https://someserver.com?filename=Gnome_1_5.png",
+		"attributes": [
+			{
+				"trait_type": "Accessory",
+				"value": "staff"
+			},
+			{
+				"trait_type": "Background",
+				"value": "daytime"
+			},
+			{
+				"trait_type": "Attack",
+				"value": 90
+			},
+			{
+				"trait_type": "Defense",
+				"value": 68
+			},
+			{
+				"trait_type": "Health",
+				"value": 13
+			},
+			{
+				"trait_type": "Building",
+				"value": 98
+			},
+			{
+				"trait_type": "Total Skill",
+				"value": 299
+			},
+			{
+				"display_type": "date",
+				"trait_type": "birthday",
+				"value": -3610875568
+			},
+			{
+				"display_type": "boost_number",
+				"trait_type": "Health Boost",
+				"value": 30
+			}
+		]
+	},
+	"6": {
+		"description": "Gnome 6 of 1000",
+		"image": "https://someserver.com?filename=Gnome_1_6.png",
+		"attributes": [
+			{
+				"trait_type": "Accessory",
+				"value": "orb"
+			},
+			{
+				"trait_type": "Background",
+				"value": "ethmoon"
+			},
+			{
+				"trait_type": "Attack",
+				"value": 27
+			},
+			{
+				"trait_type": "Defense",
+				"value": 67
+			},
+			{
+				"trait_type": "Health",
+				"value": 86
+			},
+			{
+				"trait_type": "Magic",
+				"value": 71
+			},
+			{
+				"trait_type": "Total Skill",
+				"value": 301
+			},
+			{
+				"display_type": "date",
+				"trait_type": "birthday",
+				"value": -6562540825
+			},
+			{
+				"display_type": "boost_number",
+				"trait_type": "Health Boost",
+				"value": 30
+			},
+			{
+				"display_type": "boost_number",
+				"trait_type": "Magic Boost",
+				"value": 20
+			}
+		]
+	},
+	"7": {
+		"description": "Gnome 7 of 1000",
+		"image": "https://someserver.com?filename=Gnome_1_7.png",
+		"attributes": [
+			{
+				"trait_type": "Accessory",
+				"value": "crossbow"
+			},
+			{
+				"trait_type": "Background",
+				"value": "nighttime"
+			},
+			{
+				"trait_type": "Attack",
+				"value": 33
+			},
+			{
+				"trait_type": "Defense",
+				"value": 51
+			},
+			{
+				"trait_type": "Health",
+				"value": 91
+			},
+			{
+				"trait_type": "Magic",
+				"value": 59
+			},
+			{
+				"trait_type": "Total Skill",
+				"value": 284
+			},
+			{
+				"display_type": "date",
+				"trait_type": "birthday",
+				"value": -4984075481
+			},
+			{
+				"display_type": "boost_number",
+				"trait_type": "Attack Boost",
+				"value": 50
+			}
+		]
+	},
+	"8": {
+		"description": "Gnome 8 of 1000",
+		"image": "https://someserver.com?filename=Gnome_1_8.png",
+		"attributes": [
+			{
+				"trait_type": "Accessory",
+				"value": "sword"
+			},
+			{
+				"trait_type": "Background",
+				"value": "nighttime"
+			},
+			{
+				"trait_type": "Attack",
+				"value": 80
+			},
+			{
+				"trait_type": "Defense",
+				"value": 73
+			},
+			{
+				"trait_type": "Health",
+				"value": 16
+			},
+			{
+				"trait_type": "Clairvoyance",
+				"value": 40
+			},
+			{
+				"trait_type": "Total Skill",
+				"value": 219
+			},
+			{
+				"display_type": "date",
+				"trait_type": "birthday",
+				"value": -2365443476
+			},
+			{
+				"display_type": "boost_number",
+				"trait_type": "Attack Boost",
+				"value": 10
+			}
+		]
+	},
+	"9": {
+		"description": "Gnome 9 of 1000",
+		"image": "https://someserver.com?filename=Gnome_1_9.png",
+		"attributes": [
+			{
+				"trait_type": "Accessory",
+				"value": "orb"
+			},
+			{
+				"trait_type": "Background",
+				"value": "daytime"
+			},
+			{
+				"trait_type": "Attack",
+				"value": 54
+			},
+			{
+				"trait_type": "Defense",
+				"value": 95
+			},
+			{
+				"trait_type": "Health",
+				"value": 64
+			},
+			{
+				"trait_type": "Clairvoyance",
+				"value": 68
+			},
+			{
+				"trait_type": "Total Skill",
+				"value": 351
+			},
+			{
+				"display_type": "date",
+				"trait_type": "birthday",
+				"value": -3010743721
+			},
+			{
+				"display_type": "boost_number",
+				"trait_type": "Health Boost",
+				"value": 30
+			},
+			{
+				"display_type": "boost_number",
+				"trait_type": "Clairvoyance Boost",
+				"value": 40
+			}
+		]
+	}
 }
